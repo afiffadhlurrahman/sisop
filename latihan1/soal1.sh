8732f636486a62068afd6051e5f0cc6b826aab27
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-02-12 14:07:01 +0000

    update bener

--- latihan1/soal1.sh
+++ latihan1/soal1.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
 func(){
-if [ $number -le 1 ]
-then
-	bool=false
-fi
-
-for ((i=2; i<number; i=i+1))
-do
-	if [ $(($number % $i)) -eq 0 ]
+	if [ $number -le 1 ]
 	then
 		bool=false
 	fi
-done
+
+	for ((i=2; i<number; i=i+1))
+	do
+		if [ $(($number % $i)) -eq 0 ]
+		then
+			bool=false
+		fi
+	done
 }
 
-func_gen(){
-read t
-while [ $t -gt 0 ]
-do
-	bool=true	
-	read number
-	#echo -e "This is your number : $number"
-
-	func
-	
-	echo "$bool"
-	t=$((t-1))
-done
-}
-
-
-afif="ganteng"
+afif=$ganteng
 echo -e "Hello afif $afif!"
-func_gen
+t=$#
+myArray=( "$@" )
+
+for arg in "${myArray[@]}"; do
+	bool=true
+	number=$arg
+	echo "$number"
+	func
+	echo "$bool"
+done
+
 echo "See you again"
