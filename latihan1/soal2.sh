07037ea94b8709739660db9f8e58e7b6d42569ee
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-02-12 14:14:56 +0000

    update jam

--- latihan1/soal2.sh
+++ latihan1/soal2.sh
@@ -2,6 +2,5 @@
 
 crontab -e
 
-* 0-15 * * * ls /home/afif/Downloads > /home/afif/log
-* 18-23 * * * ls /home/afif/Downloads > /home/afif/log
-
+* 0-14 * * * ls /home/afif/Downloads > /home/afif/log
+* 19-23 * * * ls /home/afif/Downloads > /home/afif/log
