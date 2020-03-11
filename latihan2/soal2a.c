f83fa97f51127c39c2c4c24885255aa73a837764
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-03-11 14:15:57 +0000

    Create soal2a.c

---
+++ latihan2/soal2a.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+int main() {
+    pid_t pid, sid;        // Variabel untuk menyimpan PID
+
+    pid = fork();     // Menyimpan PID dari Child Process
+
+    /* Keluar saat fork gagal
+    * (nilai variabel pid < 0) */
+    if (pid < 0) {
+    exit(EXIT_FAILURE);
+    }
+
+    /* Keluar saat fork berhasil
+    * (nilai variabel pid adalah PID dari child process) */
+    if (pid > 0) {
+    exit(EXIT_SUCCESS);
+    }
+
+    umask(0);
+
+    sid = setsid();
+    if (sid < 0) {
+    exit(EXIT_FAILURE);
+    }
+
+    if ((chdir("/home/afif/sisop/shift2/")) < 0) {
+    exit(EXIT_FAILURE);
+    }
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    while (1) {    
+        char *argv[5] = {"/usr/bin/wget","-P","/home/afif/sisop/shift2/", "https://picsum.photos/200", NULL};
+        execv("/usr/bin/wget", argv);    
+        sleep(5);
+    }
+}
