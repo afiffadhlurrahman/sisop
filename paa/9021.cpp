017a1e241d49f12f683a02e790c714ab5a357828
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-03-29 14:17:07 +0700

    udah bener

--- paa/9021.cpp
+++ paa/9021.cpp
@@ -1,7 +1,11 @@
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define maxNum (LL)(1e18) 
+ 
 using namespace std; 
 typedef long long int LL;
-#define maxNum (int)(1e8) 
+
 vector<LL> v; 
   
 void precompute() 
@@ -25,19 +29,16 @@ void precompute()
   
 void countNum(LL l, LL r) 
 { 
-    cout << upper_bound(v.begin(), v.end(), r) - upper_bound(v.begin(), v.end(), l - 1) << endl; 
+    printf("%lld\n",upper_bound(v.begin(), v.end(), r) - upper_bound(v.begin(), v.end(), l - 1));
 } 
 
 int main() 
 { 
     LL l, r; 
-  	
-  	while(scanf("%lld %lld",&l,&r) != EOF){
-  		memset(&v[0], 0, sizeof(v[0]) * v.size());
-  		precompute(); 
-		countNum(l, r); 
-		//printf("%lld %lld\n",l,r); 
-	}
+    precompute(); 
+    while(scanf("%lld %lld",&l,&r) != EOF){
+	countNum(l, r); 
+    }
     
     return 0; 
 }
